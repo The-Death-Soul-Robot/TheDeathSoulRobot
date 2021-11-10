2e5f071b319c17407a5e7e01544d8bcfa6fedeb9
Sumit yadav <91660827+Sumit9969@users.noreply.github.com>
2021-11-10 16:17:35 +0530

    Update logomaker.py

--- TGNRobot/modules/logomaker.py
+++ TGNRobot/modules/logomaker.py
@@ -40,7 +40,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Social_Friends_Club, {e}')
+   await event.reply(f'Error Report @World_FriendShip_zone, {e}')
 
 
 
@@ -80,7 +80,7 @@ async def lego(event):
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @Social_Friends_Club, {e}')
+   await event.reply(f'Error Report @World_FriendShip_Zone, {e}')
 
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
